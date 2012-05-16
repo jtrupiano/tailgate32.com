5062b7357f3eecef65b28d0a377110d8e353e35f
John Trupiano <jtrupiano@gmail.com>
2012-05-16 18:16:33 -0500

    Fix form clear bug.

--- public/js/tailgate32.js
+++ public/js/tailgate32.js
@@ -1,6 +1,8 @@
 $().ready(function() {
   // Set up contact us fancybox
   $('#contact-us-nav').click(function() {
+    // clear the form
+    $('#contact-us-form input[type="text"], #contact-us-form textarea').val("");
     $('#contact-us-form').show();
     $('#contactu-us-thanks').hide();
     $.fancybox.open($('#contact-us'));
@@ -10,7 +12,5 @@ $().ready(function() {
   $('#contact-us-submit').click(function() {
     $('#contact-us-form').fadeOut('slow');
     $('#contact-us-thanks').fadeIn('slow');
-    // clear the form
-    $('#contact-us-form input[type="text"], #contact-us-form textarea').val("");
   });
 });
