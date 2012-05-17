c870bf5e1fae377384d8505da386c694922c6b65
John Trupiano <jtrupiano@gmail.com>
2012-05-17 16:19:14 -0500

    Hovers for call to action.

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,13 @@
 $().ready(function() {
+  // Set up rollover for call to action
+  $('#call-to-action-link').hover(function() {
+    $('#call-to-action-shirt').attr("src", "images/shirt-hover.png");
+    $('#call-to-action-kickstart').attr("src", "images/kickstart-hover.png");
+  }, function() {
+    $('#call-to-action-shirt').attr("src", "images/shirt.png");
+    $('#call-to-action-kickstart').attr("src", "images/kickstart.png");
+  });
+
   // set fancybox defaults
   $.fancybox.defaults.beforeLoad = function() { $('#video-container').css('visibility', 'hidden'); };
   $.fancybox.defaults.afterClose = function() { $('#video-container').css('visibility', 'visible'); };
