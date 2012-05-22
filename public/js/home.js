c98d8f7f71b6372639048cd23c5f3c19df9b5da5
John Trupiano <jtrupiano@gmail.com>
2012-05-22 14:01:00 -0500

    If a team querystring parameter is provided, open the tshirt viewer to that team's shirt.

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,15 @@
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null) {
+    return "";
+  } else {
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+}
+
 $().ready(function() {
   // Set up rollover for call to action
   $('#call-to-action-link').hover(function() {
@@ -11,5 +23,13 @@ $().ready(function() {
   // set fancybox defaults
   $.fancybox.defaults.beforeLoad = function() { $('#video-container').css('visibility', 'hidden'); };
   $.fancybox.defaults.afterClose = function() { $('#video-container').css('visibility', 'visible'); };
+
+  // Open tshirt dropdown if querystring is set
+  var preselect = getParameterByName("team");
+  if (preselect != "") {
+    $('#tshirt-options').val(preselect).trigger("change");
+    $.fancybox.open($('#tshirt-viewer'));
+  }
 });
+
 
