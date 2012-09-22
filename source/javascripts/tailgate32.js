c02cc6bffdde1664675fa083bc051e55f95a6d5c
John Trupiano <jtrupiano@gmail.com>
2012-09-22 11:21:04 -0400

    Don't pull from assets.tailgate32.com anymore.

---
+++ source/javascripts/tailgate32.js
@@ -0,0 +1,16 @@
+$().ready(function() {
+  // Set up contact us fancybox
+  $('a.contact-us-link').click(function() {
+    // clear the form
+    $('#contact-us-form input[type="text"], #contact-us-form textarea').val("");
+    $('#contact-us-form').show();
+    $('#contactu-us-thanks').hide();
+    $.fancybox.open($('#contact-us'));
+  });
+
+  // Set up contact us form AJAX
+  $('#contact-us-submit').click(function() {
+    $('#contact-us-form').fadeOut('slow');
+    $('#contact-us-thanks').fadeIn('slow');
+  });
+});
