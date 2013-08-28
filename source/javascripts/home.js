fda9aa9ea814730b34df1e45208c8de9c88a9a89
John Trupiano <jtrupiano@gmail.com>
2013-08-27 19:01:01 -0500

    Open schedule if query string says to on home page.

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -1,3 +1,14 @@
+function getUrlVars() {
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
+
 $().ready(function() {
   $('#schedule-callout').click(function() {
     $.fancybox.open($('#schedule-popup'));
@@ -6,4 +17,8 @@ $().ready(function() {
   $('#schedule-contact-link').click(function() {
     $.fancybox.open($('#contact-us'));
   });
+
+  if (getUrlVars()["schedule"] == "y") {
+    $.fancybox.open($('#schedule-popup'));
+  }
 });
