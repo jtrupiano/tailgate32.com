d72df07309587156c91fa4a1f7b5d4a8ee9c6372
John Trupiano <jtrupiano@gmail.com>
2013-09-04 10:56:34 -0500

    Release Foods B-Side

--- config.rb
+++ config.rb
@@ -32,7 +32,7 @@ page "/index.html" do
   @featured_episode           = episodes.last
   @tailgate32_episodes        = [19,15,14,28].map {|i| episodes.reverse[i]}
   @featured_revvedup_episodes = [0,3,5,1].map {|i| revvedup_episodes[i]}
-  @featured_bsides            = [1,0,2,3].map {|i| bsides[i]}
+  @featured_bsides            = [0,1,2,3].map {|i| bsides[i]}
 end
 
 page "/episodes.html"
